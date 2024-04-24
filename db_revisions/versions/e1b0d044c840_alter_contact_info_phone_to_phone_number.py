b90b47d4b495bbb5fecf9cd1d9f785bd605783d6
Nargis Sultani <nargis.sultani@cfpb.gov>
2024-04-24 14:58:48 -0400

    changed alembic revision

--- db_revisions/versions/e1b0d044c840_alter_contact_info_phone_to_phone_number.py
+++ db_revisions/versions/e1b0d044c840_alter_contact_info_phone_to_phone_number.py
@@ -1,7 +1,7 @@
 """alter contact_info phone to phone_number
 
 Revision ID: e1b0d044c840
-Revises: 3f7e610035a6
+Revises: c89920be2e66
 Create Date: 2024-04-24 14:36:55.388541
 
 """
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "e1b0d044c840"
-down_revision: Union[str, None] = "3f7e610035a6"
+down_revision: Union[str, None] = "c89920be2e66"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
