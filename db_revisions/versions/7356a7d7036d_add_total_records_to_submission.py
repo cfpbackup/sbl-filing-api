2b426aa2ff53e197e1ae4e6557a18b0e0aea52b4
jmcguffee <justin.mcguffee@cfpb.gov>
2024-05-07 13:43:08 -0400

    Added total records logic, updated tests, formatting

--- db_revisions/versions/7356a7d7036d_add_total_records_to_submission.py
+++ db_revisions/versions/7356a7d7036d_add_total_records_to_submission.py
@@ -9,11 +9,10 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision: str = '7356a7d7036d'
-down_revision: Union[str, None] = '5492f53d1fa5'
+revision: str = "7356a7d7036d"
+down_revision: Union[str, None] = "5492f53d1fa5"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
